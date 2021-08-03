1169a6efa2a797c57170dedd82f0bb10aab593a8
CelesteRenteria <celeste.renteria.is@unipolidgo.edu.mx>
2021-08-03 01:18:18 -0500

    commit

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //await app.listen(3000);
-  app.use(cors());
+  //app.use(cors());
   await app.listen(process.env.PORT || 8080);
   
 }
